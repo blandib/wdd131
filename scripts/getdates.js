75a0913507b3b32c9dcdc1d54bd2c33f177a7cfc
BlandineB <blandinebukasa118@gmail.com>
2025-05-07 11:55:58 +0200

    Update the change

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically set the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+
+document.getElementById("lastModified").textContent = "Last Updated: " + document.lastModified;
